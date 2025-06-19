66ee62afae795169f36b9c516b6019f5db43ad30
Hemanthbrh <hemanthbrh87@gmail.com>
2025-06-19 22:20:39 +0530

    update

---
+++ Banking Applicatiom/Banking-Applicatiom/src/main/java/com/bank/applicatiom/BankingApplicatiomApplication.java
@@ -0,0 +1,13 @@
+package com.bank.applicatiom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApplicatiomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApplicatiomApplication.class, args);
+	}
+
+}
---
+++ Banking Applicatiom/Banking-Applicatiom/src/test/java/com/bank/applicatiom/BankingApplicatiomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.applicatiom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicatiomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
